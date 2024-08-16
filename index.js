556d235e9efc4dd1dc12c5ee8c48c20b7d93cb31
holyrag <166366806+holyrag@users.noreply.github.com>
2024-08-16 22:24:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const code = require('./pair');
+
+// Set the maximum number of listeners to 500 (optional)
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the current directory
+app.use('/code', code);
+app.use('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`YouTube: https://www.youtube.com/@rage4721\nTelegram: t.me/rageboy1\nGitHub: https://github.com/holyrag\nInstagram: https://www.instagram.com/pk123_yy6\n\nServer running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
